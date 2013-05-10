a912552d90262408fff438383594f24468692a67
Raynos <raynos2@gmail.com>
2013-05-10 10:08:40 -0700

    removeduuid dep

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var persistent = require("persistent-hash-trie")
 var isObject = require("is-object")
 var Keys = require("object-keys")
-var uuid = require("uuid")
 
 var Trie = persistent.Trie
 var assoc = persistent.assoc
